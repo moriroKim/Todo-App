86d8475bd6db271339b75e4753ba614d2629bad9
moriroKim <moriroKim@gmail.com>
2024-07-29 14:13:24 +0900

    todolist 리액트 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,13 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 0, content: "123" },
-    { id: 1, content: "코딩 공부하기" },
-    { id: 2, content: "잠 자기" },
-  ]);
-
-  return (
-    <>
-      <TodoList todoList={todoList} setTodoList={setTodoList} />
-      <hr />
-      <TodoInput todoList={todoList} setTodoList={setTodoList} />
-    </>
-  );
-}
-
-function TodoInput({ todoList, setTodoList }) {
-  const [inputValue, setInputValue] = useState("");
-
-  return (
-    <>
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          const newTodo = { id: Number(new Date()), content: inputValue };
-          const newTodoList = [...todoList, newTodo];
-          setTodoList(newTodoList);
-          setInputValue("");
-        }}
-      >
-        추가하기
-      </button>
-    </>
-  );
-}
-
-function TodoList({ todoList, setTodoList }) {
-  return (
-    <ul>
-      {todoList.map((todo) => (
-        <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
-      ))}
-    </ul>
-  );
-}
-
-function Todo({ todo, setTodoList }) {
-  const [inputValue, setInputValue] = useState("");
-  return (
-    <li>
-      {todo.content}
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
-            )
-          );
-        }}
-      >
-        수정
-      </button>
-      <button
-        onClick={() => {
-          setTodoList((prev) => {
-            return prev.filter((el) => el.id !== todo.id);
-          });
-        }}
-      >
-        삭제
-      </button>
-    </li>
-  );
+    return (
+        <>
+            <TodoList />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Form = ({ createTodo }) => {
+    const [value, setValue] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        createTodo(value);
+        setValue('');
+    };
+    return (
+        <form className="mb-4 font-primary w-full" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="outline-none bg-transparent border border-gray-500 p-4 w-[300px] text-white mb-8 rounded placeholder:text-gray-300"
+                placeholder="what task do you have today?"
+                onChange={(e) => setValue(e.target.value)}
+                value={value}
+            />
+            <button
+                type="submit"
+                className="bg-gray-400 border border-gray-500 p-4 text-white cursor-pointer rounded ml-2"
+            >
+                Add Task
+            </button>
+        </form>
+    );
+};
+
+export default Form;
